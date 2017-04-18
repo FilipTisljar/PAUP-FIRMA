64f66cc4f83d06b78748a58bc2dfe85f15c0d2ae
kedmenecr <kedmenec.roberto@gmail.com>
2017-04-18 20:08:32 +0200

    Spajanje baze
    Spajanje baze s projektom

---
+++ WebApplication1/Models/BazaDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class BazaDB : DbContext
+    {
+        public DbSet<BazaDB> Baza { get; set; }
+    }
+}
